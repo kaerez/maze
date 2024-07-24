d15baa637b9458a0989793c62f2862ba3467d8f0
kaerez <kaerez@users.noreply.github.com>
2024-07-24 21:16:12 +0300

    Update maze.py

--- streamlit/maze.py
+++ streamlit/maze.py
@@ -88,16 +88,14 @@ def draw_maze(cells, draw):
                 draw.line([(x1, y1), (x1, y2)], fill=BLACK)
 
 def draw_entrance(draw, entrance):
-    x = entrance.x * CELL_SIZE + CELL_SIZE // 2
-    y = entrance.y * CELL_SIZE + CELL_SIZE // 2
-    radius = CELL_SIZE // 4
-    draw.ellipse([(x - radius, y - radius), (x + radius, y + radius)], fill=GREEN)
+    x1 = entrance.x * CELL_SIZE + CELL_SIZE
+    y1 = entrance.y * CELL_SIZE + CELL_SIZE
+    draw.rectangle([x1, y1, x1 + CELL_SIZE, y1 + CELL_SIZE], fill=GREEN)
 
 def draw_exit(draw, exit):
-    x = exit.x * CELL_SIZE + CELL_SIZE + CELL_SIZE // 2
-    y = exit.y * CELL_SIZE + CELL_SIZE + CELL_SIZE // 2
-    radius = CELL_SIZE // 4
-    draw.ellipse([(x - radius, y - radius), (x + radius, y + radius)], fill=RED)
+    x1 = exit.x * CELL_SIZE + CELL_SIZE
+    y1 = exit.y * CELL_SIZE + CELL_SIZE
+    draw.rectangle([x1, y1, x1 + CELL_SIZE, y1 + CELL_SIZE], fill=RED)
 
 def draw_path(draw, entrance, exit):
     current = exit
